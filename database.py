a7678240008d7ef5dac0443d45434c96c7cbdfed
k4dudev <k4dudev@gmail.com>
2026-05-29 00:24:56 -0300

    atual

--- database.py
+++ database.py
@@ -4,12 +4,15 @@ from sqlalchemy.orm import sessionmaker
 
 #DATABASE_URL = "sqlite:///./banco.db"
 #DATABASE_URL = "postgresql://usuario:senha@host:5432/banco"
-DATABASE_URL = postgresql://postgres:@N4sc1m3nt0@db.ihrwdfyyivctirrymgkq.supabase.co:5432/postgres
+DATABASE_URL = "postgresql://postgres:N4sc1m3nt0@db.ihrwdfyyivctirrymgkq.supabase.co:5432/postgres"
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,

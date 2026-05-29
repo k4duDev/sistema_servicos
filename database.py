6deeb67b0172d1fd91b908c9a49e6ffee5aa5f6b
k4dudev <k4dudev@gmail.com>
2026-05-28 23:55:54 -0300

    update

--- database.py
+++ database.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./banco.db"
+#DATABASE_URL = "sqlite:///./banco.db"
 #DATABASE_URL = "postgresql://usuario:senha@host:5432/banco"
+DATABASE_URL = postgresql://postgres:[@N4sc1m3nt0]@db.ihrwdfyyivctirrymgkq.supabase.co:5432/postgres
 
 engine = create_engine(
     DATABASE_URL,
